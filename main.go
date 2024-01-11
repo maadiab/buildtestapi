e9c25c1c5b6c422b21d4c3955c15c8cc3a258dcf
maadiab <mohanad.dev@gmail.com>
2024-01-11 23:37:57 +0300

    add function

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 // Model for Course - file
 type Course struct {
 	CourseId    string  `json:"courseid"`
@@ -18,6 +24,48 @@ type Author struct {
 var courses []Course
 
 // Middleware, helper - file
+func (c *Course) IsEmpty() bool {
+	return c.CourseId == "" && c.CourseName == ""
+}
+
+// Controller - file
+func ServeHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>This is an api</h1>"))
+}
+
+func GetAllData(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("This is the data: ")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
+}
+
+func GetDatabyId(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("This is the data by id: ")
+
+	w.Header().Set("Content-Type", "application/json")
+
+	// grab id from courses
+	// params := mux.Vars(r)
+
+	// loop throughh courses and select the course and return that course
+	// for _, course := range courses {
+	// 	if course.CourseId == params["id"] {
+
+	// 	}
+	// }
+
+}
+
+// Handlers - file
+
 func main() {
+
+	http.HandleFunc("/", ServeHome)
+	http.HandleFunc("/getall", GetAllData)
+	http.HandleFunc("/getid", GetDatabyId)
+
+	http.ListenAndServe(":8090", nil)
 
 }
