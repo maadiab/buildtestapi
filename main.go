0cc818640b56f7299038a9624f361dd44e6297d3
maadiab <mohanad.dev@gmail.com>
2024-01-12 15:14:09 +0300

    add some functions

--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
 // Model for Course - file
@@ -47,14 +52,65 @@ func GetDatabyId(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	// grab id from courses
-	// params := mux.Vars(r)
+	params := mux.Vars(r)
 
 	// loop throughh courses and select the course and return that course
-	// for _, course := range courses {
-	// 	if course.CourseId == params["id"] {
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("There is no course with this id !")
+	return
+}
 
-	// 	}
-	// }
+func CreateData(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Create one course !")
+
+	w.Header().Set("Content-Type", "application/json")
+
+	// What if no data
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please Enter Some Data")
+		return
+	}
+	// What if data like {}
+
+	var course Course
+	_ = json.NewDecoder(r.Body).Decode(&course)
+
+	if course.IsEmpty() {
+
+		json.NewEncoder(w).Encode("you send an empty json")
+		return
+	}
+
+	// generate random id and add course to the courses
+
+	rand.Seed(time.Now().Unix())
+	course.CourseId = strconv.Itoa(rand.Intn(100))
+
+	courses = append(courses, course)
+
+	json.NewEncoder(w).Encode(course)
+	return
+
+}
+
+// update data by id
+
+func updateDataById(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("updating data")
+
+	w.Header().Set("Content-Type", "aplication/json")
+
+	// retreive id from user
+
+	// params := mux.Vars(r)
+
+	//loop, id, remove, add value again in this fake DB
 
 }
 
